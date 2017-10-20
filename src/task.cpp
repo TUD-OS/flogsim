121ec650548de556c6672b5a23291c1ae61a579e
Maksym Planeta <mcsim.planeta@gmail.com>
2017-10-20 18:26:59 +0200

    [Refactoring] move some methods auto of task.hpp

---
+++ src/task.cpp
@@ -0,0 +1,72 @@
+#include "task.hpp"
+
+bool LogP::RecvTask::execute(Timeline &timeline, TaskQueue &tq) const
+{
+  auto &cpu = timeline.per_cpu_time[receiver()];
+
+  // Calculate time for CpuTime
+  Time cpu_last = cpu.cpu_events.get_last_or_zero();
+  Time recv_last = cpu.recv_gaps.get_last_or_zero();
+
+  auto start_time = std::max({cpu_last, recv_last, tq.now()});
+
+  if (start_time > tq.now()) {
+    tq.schedule(std::make_shared<RecvTask>(start_time, sender(), receiver()));
+    return false;
+  }
+
+  RecvGap rg{start_time, sender()};
+  CpuEvent cpu_event{start_time};
+
+  cpu.recv_gaps.push_back(rg);
+  cpu.cpu_events.push_back(cpu_event);
+
+  return true;
+}
+
+bool LogP::MsgTask::execute(Timeline &timeline, TaskQueue &tq) const
+{
+  // Calculate time when receive task can be scheduled
+  auto recv_time = tq.now() + LogP::Model::get().L;
+
+  tq.schedule(std::make_shared<RecvTask>(recv_time, sender(), receiver()));
+  return true;
+}
+
+bool LogP::SendTask::execute(Timeline &timeline, TaskQueue &tq) const
+{
+  auto &cpu = timeline.per_cpu_time[sender()];
+
+  // Calculate time for CpuTime
+  Time cpu_last = cpu.cpu_events.get_last_or_zero();
+  Time send_last = cpu.send_gaps.get_last_or_zero();
+
+  auto start_time = std::max({cpu_last, send_last, tq.now()});
+
+  if (start_time > tq.now()) {
+    tq.schedule(std::make_shared<SendTask>(start_time, sender(), receiver()));
+    return false;
+  }
+
+  SendGap sg{start_time};
+  CpuEvent cpu_event{start_time};
+
+  cpu.send_gaps.push_back(sg);
+  cpu.cpu_events.push_back(cpu_event);
+
+  tq.schedule(std::make_shared<MsgTask>(cpu_event.end(), sender(), receiver()));
+  return true;
+}
+
+bool LogP::FinishTask::execute(Timeline &timeline, TaskQueue &tq) const
+{
+  auto &cpu = timeline.per_cpu_time[sender()];
+
+  // Calculate time for CpuTime
+  auto cpu_last =  cpu.cpu_events.back();
+
+  cpu.finish.push_back(FinishEvent(cpu_last.end()));
+
+  timeline.update_total_time(cpu.finish.back().end());
+  return true;
+}
--- src/task.hpp
+++ src/task.hpp
@@ -78,29 +78,7 @@ public:
     Task(time, sender, receiver)
   {}
 
-  bool execute(Timeline &timeline, TaskQueue &tq) const override final
-  {
-    auto &cpu = timeline.per_cpu_time[receiver()];
-
-    // Calculate time for CpuTime
-    Time cpu_last = cpu.cpu_events.get_last_or_zero();
-    Time recv_last = cpu.recv_gaps.get_last_or_zero();
-
-    auto start_time = std::max({cpu_last, recv_last, tq.now()});
-
-    if (start_time > tq.now()) {
-      tq.schedule(std::make_shared<RecvTask>(start_time, sender(), receiver()));
-      return false;
-    }
-
-    RecvGap rg{start_time, sender()};
-    CpuEvent cpu_event{start_time};
-
-    cpu.recv_gaps.push_back(rg);
-    cpu.cpu_events.push_back(cpu_event);
-
-    return true;
-  }
+  bool execute(Timeline &timeline, TaskQueue &tq) const override final;
 
   void notify(Collective &coll, TaskQueue &tq) override final
   {
@@ -121,14 +99,7 @@ public:
   {
   }
 
-  bool execute(Timeline &timeline, TaskQueue &tq) const override final
-  {
-    // Calculate time when receive task can be scheduled
-    auto recv_time = tq.now() + LogP::Model::get().L;
-
-    tq.schedule(std::make_shared<RecvTask>(recv_time, sender(), receiver()));
-    return true;
-  }
+  bool execute(Timeline &timeline, TaskQueue &tq) const override final;
 
   void notify(Collective &coll, TaskQueue &tq) override final
   {
@@ -151,30 +122,7 @@ public:
   {
   }
 
-  bool execute(Timeline &timeline, TaskQueue &tq) const override final
-  {
-    auto &cpu = timeline.per_cpu_time[sender()];
-
-    // Calculate time for CpuTime
-    Time cpu_last = cpu.cpu_events.get_last_or_zero();
-    Time send_last = cpu.send_gaps.get_last_or_zero();
-
-    auto start_time = std::max({cpu_last, send_last, tq.now()});
-
-    if (start_time > tq.now()) {
-      tq.schedule(std::make_shared<SendTask>(start_time, sender(), receiver()));
-      return false;
-    }
-
-    SendGap sg{start_time};
-    CpuEvent cpu_event{start_time};
-
-    cpu.send_gaps.push_back(sg);
-    cpu.cpu_events.push_back(cpu_event);
-
-    tq.schedule(std::make_shared<MsgTask>(cpu_event.end(), sender(), receiver()));
-    return true;
-  }
+  bool execute(Timeline &timeline, TaskQueue &tq) const override final;
 
   void notify(Collective &coll, TaskQueue &tq) override final
   {
@@ -196,18 +144,7 @@ public:
   {
   }
 
-  bool execute(Timeline &timeline, TaskQueue &tq) const override final
-  {
-    auto &cpu = timeline.per_cpu_time[sender()];
-
-    // Calculate time for CpuTime
-    auto cpu_last =  cpu.cpu_events.back();
-
-    cpu.finish.push_back(FinishEvent(cpu_last.end()));
-
-    timeline.update_total_time(cpu.finish.back().end());
-    return true;
-  }
+  bool execute(Timeline &timeline, TaskQueue &tq) const override final;
 
   void notify(Collective &coll, TaskQueue &tq) override final
   {
