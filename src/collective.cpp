881110ac9395ebda1640fb7d8fb8c5d5cc9e4a01
Maksym Planeta <mcsim.planeta@gmail.com>
2017-10-25 13:15:52 +0200

    Add initial version for checked tree bcast

--- src/collective.cpp
+++ src/collective.cpp
@@ -82,6 +82,161 @@ public:
   }
 };
 
+class CheckedCorrectedTreeBroadcast : public Collective
+{
+  int k;
+  int nodes;
+
+  struct Ring
+  {
+    std::vector<bool> done;
+    std::vector<int> offs;
+    int inc;
+
+    Ring(int nodes, int inc):
+      done(nodes), offs(nodes), inc(inc)
+    {}
+  };
+
+  Ring left, right;
+  std::vector<bool> tree_done;
+
+  static Tag tree_tag() { return Tag(0); }
+  static Tag left_ring_tag() { return Tag(2); }
+  static Tag right_ring_tag() { return Tag(4); }
+
+  void post_tree_sends(int sender, TaskQueue &tq)
+  {
+    if (tree_done[sender]) {
+      return;
+    }
+
+    int lvl = int(std::log(sender + 1) / std::log(k));
+    for (int i = 1; i <= k; i++) {
+      int recv = sender + i * std::pow(k, lvl);
+      if (recv < nodes) {
+        tq.schedule(LogP::SendStartTask::make_new(tree_tag(), tq.now(), sender, recv));
+      }
+    }
+
+    tree_done[sender] = true;
+
+    if (left.done[sender] && right.done[sender]) {
+      tq.schedule(LogP::FinishTask::make_new(sender));
+    }
+  }
+
+  bool check_done(int sender)
+  {
+    int left_shift = left.offs[sender];
+    int right_shift = right.offs[sender];
+
+    if (left_shift == 0 || right_shift == 0) {
+      // Need to send at least once
+      return false;
+    }
+
+    if ((left_shift + nodes) % nodes == (right_shift + nodes) % nodes) {
+      left.done[sender] = true;
+      right.done[sender] = true;
+
+      return true;
+    }
+
+    return left.done[sender] && right.done[sender];
+  }
+
+  void post_ring_sends(int sender, TaskQueue &tq)
+  {
+    if (!left.done[sender]) {
+      int left_shift = left.offs[sender] + left.inc;
+
+      if (check_done(sender)) {
+        return;
+      }
+
+      left.offs[sender] = left_shift;
+
+      // Send correction messages to the left
+      int recv = (sender + left_shift + nodes) % nodes;
+      tq.schedule(LogP::SendStartTask::make_new(left_ring_tag(), tq.now(), sender, recv));
+    }
+
+    if (!right.done[sender]) {
+      // Send correction messages to the right
+      int right_shift = right.offs[sender] + right.inc;
+
+      if (check_done(sender)) {
+        return;
+      }
+
+      right.offs[sender] = right_shift;
+
+      int recv = (sender + right_shift + nodes) % nodes;
+      tq.schedule(LogP::SendStartTask::make_new(right_ring_tag(), tq.now(), sender, recv));
+    }
+
+    if (left.done[sender] && right.done[sender]) {
+      tq.schedule(LogP::FinishTask::make_new(sender));
+    }
+  }
+
+public:
+  CheckedCorrectedTreeBroadcast()
+    : k(Configuration::get().k),
+      nodes(Configuration::get().P),
+      tree_done(nodes),
+      left(nodes, -1),
+      right(nodes, +1)
+  {}
+
+  virtual void accept(const LogP::SendGapEndTask& task, TaskQueue& tq)
+  {
+    post_ring_sends(task.sender(), tq);
+  }
+
+  virtual void accept(const LogP::RecvEndTask& task, TaskQueue& tq)
+  {
+    int me = task.receiver();
+
+    if (task.tag() == tree_tag()) {
+      // Got message from the parent, need to propagate tree
+      // communication
+      post_tree_sends(me, tq);
+    } else if (task.tag() == right_ring_tag()) {
+      // Received correction from the left
+      if (left.offs[me] == 0) {
+        // Need to send a reply at least once
+        tq.schedule(LogP::SendStartTask::make_new(left_ring_tag(),
+                                                  tq.now(), me, task.sender()));
+      }
+      if (left.offs[me] > 0) {
+        left.done[me] = true;
+      }
+    } else if (task.tag() == left_ring_tag()) {
+      // Received correction from the right
+      if (right.offs[me] == 0) {
+        // Need to send a reply at least once
+        right.offs[me] ++;
+        tq.schedule(LogP::SendStartTask::make_new(right_ring_tag(),
+                                                  tq.now(), me, task.sender()));
+      }
+      if (right.done[me] > 0) {
+        right.done[me] = true;
+      }
+    } else {
+      assert(false);
+    }
+  }
+
+  void populate(TaskQueue &tq) override
+  {
+    int root = 0;
+    post_tree_sends(root, tq);
+    post_ring_sends(root, tq);
+  }
+};
+
 std::unique_ptr<Collective> Collective::create()
 {
   auto &conf = Configuration::get();
@@ -90,6 +245,8 @@ std::unique_ptr<Collective> Collective::create()
     return std::make_unique<BinaryBroadcast>();
   } else if (conf.collective == "correctedtree_bcast") {
     return std::make_unique<CorrectedTreeBroadcast>();
+  } else if (conf.collective == "checkedcorrectedtree_bcast") {
+    return std::make_unique<CheckedCorrectedTreeBroadcast>();
   } else {
     throw std::invalid_argument("Collective does not exist:" +
                                 conf.collective);
