3f8162fa61532997109aeb797d10d9462fdc8b5a
Maksym Planeta <mcsim.planeta@gmail.com>
2017-11-01 15:31:44 +0100

    Add builder methods to configuration

--- include/configuration.hpp
+++ include/configuration.hpp
@@ -18,4 +18,31 @@ struct Configuration
   // Collectives parameters
   std::string collective;
   int k;
+
+  // Builder methods
+  Configuration &LogP(int L, int o, int g, int P)
+  {
+    this->L = L;
+    this->o = o;
+    this->g = g;
+    this->P = P;
+
+    return *this;
+  }
+
+  Configuration &faults(const std::string &fault_injector, int F = 0)
+  {
+    this->fault_injector = fault_injector;
+    this->F = F;
+
+    return *this;
+  }
+
+  Configuration(int k, uint64_t limit)
+    : log_prefix(), verbose(false), limit(limit), collective(), k(k)
+  {}
+
+  Configuration()
+    : Configuration(2, UINT64_MAX)
+  {}
 };
