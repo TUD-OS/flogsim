316e7045c0704c382776a88603c88beba3e3fe6d
Maksym Planeta <mcsim.planeta@gmail.com>
2017-11-02 23:13:41 +0100

    [CCTB] Add node object

--- collectives/checked_correctedtree_bcast.hpp
+++ collectives/checked_correctedtree_bcast.hpp
@@ -10,109 +10,117 @@ class CheckedCorrectedTreeBroadcast : public Collective
   int k;
   int nodes;
 
-  std::vector<bool> tree_recv;
-  std::vector<bool> tree_sent;
-  std::vector<bool> left_done;
-  std::vector<bool> right_done;
-  std::vector<int> left_offs;
-  std::vector<int> right_offs;
-  std::vector<bool> all_done;
-  std::vector<int> right_min_recv; // closest sender from right with "left" tag
-  std::vector<int> left_min_recv; // closest sender from left with "right" tag
+  struct Node
+  {
+    int all_done : 1;
+    int tree_recv : 1;
+    int tree_sent : 1;
+
+    int left_done : 1;
+
+    int right_done : 1;
+
+    int left_offs, right_offs;
+    int right_min_recv; // closest sender from right with "left" tag
+    int left_min_recv; // closest sender from left with "right" tag
+    int id;
+  };
+
+  std::vector<Node> nodeset;
 
   static Tag tree_tag() { return Tag(0); }
   static Tag left_ring_tag() { return Tag(2); }
   static Tag right_ring_tag() { return Tag(4); }
 
-  bool post_tree_sends(int sender, TaskQueue &tq)
+  bool post_tree_sends(Node &node, TaskQueue &tq)
   {
-    if (tree_sent[sender]) {
+    if (node.tree_sent) {
       return false;
     }
 
     bool sent = false;
-    int lvl = int(std::log(sender + 1) / std::log(k));
+    int lvl = int(std::log(node.id + 1) / std::log(k));
     for (int i = 1; i <= k; i++) {
-      int recv = sender + i * std::pow(k, lvl);
+      int recv = node.id + i * std::pow(k, lvl);
       if (recv < nodes) {
-        tq.schedule(SendStartTask::make_new(tree_tag(), tq.now(), sender, recv));
+        tq.schedule(SendStartTask::make_new(tree_tag(), tq.now(), node.id, recv));
         sent = true;
       }
     }
 
-    tree_sent[sender] = true;
+    node.tree_sent = true;
     return sent;
   }
 
-  void post_ring_sends(int node, TaskQueue &tq)
+  void post_ring_sends(Node &node, TaskQueue &tq)
   {
-    if (!tree_recv[node]) {
+    if (!node.tree_recv) {
       return;
     }
 
-    if (!left_done[node] && !right_done[node]) {
-      if (left_offs[node] <= right_offs[node]) {
-        int offs = ++left_offs[node];
-        int recv = (2 * nodes + node - offs) % nodes;
-        tq.schedule(SendStartTask::make_new(left_ring_tag(), tq.now(), node, recv));
-        if (right_min_recv[node] <= left_offs[node]) {
-          left_done[node] = true;
+    if (!node.left_done && !node.right_done) {
+      if (node.left_offs <= node.right_offs) {
+        int offs = ++node.left_offs;
+        int recv = (2 * nodes + node.id - offs) % nodes;
+        tq.schedule(SendStartTask::make_new(left_ring_tag(), tq.now(), node.id, recv));
+        if (node.right_min_recv <= node.left_offs) {
+          node.left_done = true;
         }
       } else {
-        int offs = ++right_offs[node];
-        int recv = (2 * nodes + node + offs) % nodes;
-        tq.schedule(SendStartTask::make_new(right_ring_tag(), tq.now(), node, recv));
-        if (left_min_recv[node] <= right_offs[node]) {
-          right_done[node] = true;
+        int offs = ++node.right_offs;
+        int recv = (2 * nodes + node.id + offs) % nodes;
+        tq.schedule(SendStartTask::make_new(right_ring_tag(), tq.now(), node.id, recv));
+        if (node.left_min_recv <= node.right_offs) {
+          node.right_done = true;
         }
       }
-    } else if (!left_done[node]) {
-      int offs = ++left_offs[node];
-      int recv = (2 * nodes + node - offs) % nodes;
-      tq.schedule(SendStartTask::make_new(left_ring_tag(), tq.now(), node, recv));
-      if (right_min_recv[node] <= left_offs[node]) {
-        left_done[node] = true;
+    } else if (!node.left_done) {
+      int offs = ++node.left_offs;
+      int recv = (2 * nodes + node.id - offs) % nodes;
+      tq.schedule(SendStartTask::make_new(left_ring_tag(), tq.now(), node.id, recv));
+      if (node.right_min_recv <= node.left_offs) {
+        node.left_done = true;
       }
-    } else if (!right_done[node]) {
-      int offs = ++right_offs[node];
-      int recv = (2 * nodes + node + offs) % nodes;
-      tq.schedule(SendStartTask::make_new(right_ring_tag(), tq.now(), node, recv));
-      if (left_min_recv[node] <= right_offs[node]) {
-        right_done[node] = true;
+    } else if (!node.right_done) {
+      int offs = ++node.right_offs;
+      int recv = (2 * nodes + node.id + offs) % nodes;
+      tq.schedule(SendStartTask::make_new(right_ring_tag(), tq.now(), node.id, recv));
+      if (node.left_min_recv <= node.right_offs) {
+        node.right_done = true;
       }
     } else {
-      assert(left_done[node] && right_done[node]);
+      assert(node.left_done && node.right_done);
 
-      tq.schedule(FinishTask::make_new(node));
-      all_done[node] = true;
+      tq.schedule(FinishTask::make_new(tq.now(), node.id));
+      node.all_done = true;
     }
   }
 
-  void do_tree_phase(int node, TaskQueue &tq)
+  void do_tree_phase(Node &node, TaskQueue &tq)
   {
     // Got message from the parent, need to propagate tree
     // communication
-    if (tree_recv[node]) {
+    if (node.tree_recv) {
       return;
     }
 
-    tree_recv[node] = true;
+    node.tree_recv = true;
     post_tree_sends(node, tq);
 
     {
-      assert(left_offs[node] == 0);
+      assert(node.left_offs == 0);
       // Send left
-      int offs = ++left_offs[node];
-      int recv = (2 * nodes + node - offs) % nodes;
-      tq.schedule(SendStartTask::make_new(left_ring_tag(), tq.now(), node, recv));
+      int offs = ++node.left_offs;
+      int recv = (2 * nodes + node.id - offs) % nodes;
+      tq.schedule(SendStartTask::make_new(left_ring_tag(), tq.now(), node.id, recv));
     }
 
     {
-      assert(right_offs[node] == 0);
+      assert(node.right_offs == 0);
       // Send right
-      int offs = ++right_offs[node];
-      int recv = (2 * nodes + node + offs) % nodes;
-      tq.schedule(SendStartTask::make_new(right_ring_tag(), tq.now(), node, recv));
+      int offs = ++node.right_offs;
+      int recv = (2 * nodes + node.id + offs) % nodes;
+      tq.schedule(SendStartTask::make_new(right_ring_tag(), tq.now(), node.id, recv));
     }
 
   }
@@ -121,50 +129,46 @@ public:
     : Collective(),
       k(Globals::get().conf().k),
       nodes(Globals::get().model().P),
-      tree_recv(nodes),
-      tree_sent(nodes),
-      left_done(nodes),
-      right_done(nodes),
-      left_offs(nodes),
-      right_offs(nodes),
-      all_done(nodes),
-      left_min_recv(nodes, nodes),
-      right_min_recv(nodes, nodes)
-  {}
+      nodeset(nodes)
+  {
+    for (int i = 0; i < nodes; i++) {
+      nodeset[i].id = i;
+    }
+  }
 
   virtual void accept(const SendEndTask &task, TaskQueue &tq)
   {
-    int node = task.sender();
+    Node &node = nodeset[task.sender()];
 
-    if (all_done[node])  {
+    if (node.all_done)  {
       return;
     }
 
-    if ((right_min_recv[node] <= left_offs[node]) && (left_offs[node] > 1)) {
-      left_done[node] = true;
-      tq.cancel_pending_sends(node, left_ring_tag());
+    if ((node.right_min_recv <= node.left_offs) && (node.left_offs > 1)) {
+      node.left_done = true;
+      tq.cancel_pending_sends(node.id, left_ring_tag());
     }
 
-    if ((left_min_recv[node] <= right_offs[node]) && (right_offs[node] > 1)) {
-      right_done[node] = true;
-      tq.cancel_pending_sends(node, right_ring_tag());
+    if ((node.left_min_recv <= node.right_offs) && (node.right_offs > 1)) {
+      node.right_done = true;
+      tq.cancel_pending_sends(node.id, right_ring_tag());
     }
 
-    if (tq.now_empty(node)) {
+    if (tq.now_empty(node.id)) {
       post_ring_sends(node, tq);
     }
 
-    if (left_done[node] && right_done[node]) {
-      tq.schedule(FinishTask::make_new(node));
-      all_done[node] = true;
+    if (node.left_done && node.right_done) {
+      tq.schedule(FinishTask::make_new(tq.now(), node.id));
+      node.all_done = true;
     }
   }
 
   virtual void accept(const RecvEndTask& task, TaskQueue& tq)
   {
-    int node = task.receiver();
+    Node &node = nodeset[task.receiver()];
 
-    if (all_done[node])  {
+    if (node.all_done)  {
       return;
     }
 
@@ -172,46 +176,46 @@ public:
       do_tree_phase(node, tq);
     } else if (task.tag() == left_ring_tag()) {
       do_tree_phase(node, tq);
-      int dist = std::min((nodes + node - task.sender()) % nodes,
-                          (nodes + task.sender() - node) % nodes);
-      left_min_recv[node] = std::min(left_min_recv[node], dist);
-      if (tq.now_empty(node)) {
+      int dist = std::min((nodes + node.id - task.sender()) % nodes,
+                          (nodes + task.sender() - node.id) % nodes);
+      node.left_min_recv = std::min(node.left_min_recv, dist);
+      if (tq.now_empty(node.id)) {
         post_ring_sends(node, tq);
-      } else if (!right_done[node] && (left_min_recv[node] <= right_offs[node])) {
-        right_done[node] = true;
-        if (right_offs[node] > 1) {
-          tq.cancel_pending_sends(node, right_ring_tag());
+      } else if (!node.right_done && (node.left_min_recv <= node.right_offs)) {
+        node.right_done = true;
+        if (node.right_offs > 1) {
+          tq.cancel_pending_sends(node.id, right_ring_tag());
         }
       }
     } else if (task.tag() == right_ring_tag()) {
       do_tree_phase(node, tq);
-      int dist = std::min((nodes + node - task.sender()) % nodes,
-                          (nodes + task.sender() - node) % nodes);
-      right_min_recv[node] = std::min(right_min_recv[node], dist);
-      if (tq.now_empty(node)) {
+      int dist = std::min((nodes + node.id - task.sender()) % nodes,
+                          (nodes + task.sender() - node.id) % nodes);
+      node.right_min_recv = std::min(node.right_min_recv, dist);
+      if (tq.now_empty(node.id)) {
         post_ring_sends(node, tq);
-      } else if (!left_done[node] && (right_min_recv[node] <= left_offs[node])) {
-        left_done[node] = true;
-        if (left_offs[node] > 1) {
-          tq.cancel_pending_sends(node, left_ring_tag());
+      } else if (!node.left_done && (node.right_min_recv <= node.left_offs)) {
+        node.left_done = true;
+        if (node.left_offs > 1) {
+          tq.cancel_pending_sends(node.id, left_ring_tag());
         }
       }
     } else {
       assert(false);
     }
 
-    if (left_done[node] && right_done[node]) {
-      tq.schedule(FinishTask::make_new(node));
-      tq.cancel_pending_sends(node, left_ring_tag());
-      tq.cancel_pending_sends(node, right_ring_tag());
-      all_done[node] = true;
+    if (node.left_done && node.right_done) {
+      tq.schedule(FinishTask::make_new(tq.now(), node.id));
+      tq.cancel_pending_sends(node.id, left_ring_tag());
+      tq.cancel_pending_sends(node.id, right_ring_tag());
+      node.all_done = true;
     }
   }
 
   void populate(TaskQueue &tq) override
   {
-    int root = 0;
-    tree_recv[root] = true;
+    Node &root = nodeset[0];
+    root.tree_recv = true;
     post_tree_sends(root, tq);
     post_ring_sends(root, tq);
   }
