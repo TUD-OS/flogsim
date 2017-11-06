ebe14dadf9425a1499623bef07fb8b3f9e509b34
Maksym Planeta <mcsim.planeta@gmail.com>
2017-11-06 14:07:06 +0100

    Update failure free test case
    Add plot with number of messages

--- script/plot_failure_free.R
+++ script/plot_failure_free.R
@@ -28,13 +28,15 @@ output.pdf <- opt$options$output
 
 log.df <- fread(log.filename)
 
-pdf(output.pdf, 16, 9)
+node.list <- 2^c(1:13)
+
+pdf(output.pdf, 8, 5)
 for (cur.L in unique(log.df$L)) {
     ymax = max(log.df$TotalRuntime)
     p <- ggplot(log.df[L == cur.L], aes(x = P, y = TotalRuntime, color = Collective)) +
         geom_point() + geom_line() +
-        scale_x_log10(breaks = 2^c(1:11) - 1, labels = 2^c(1:11) - 1) +
-        ylim(0, ymax) +
+        scale_x_log10(breaks = node.list - 1, labels = node.list - 1) +
+        ylim(0, ymax) + ylab("Total Runtime") +
         ggtitle("Scalability in failure free case",
                 subtitle=sprintf("L = %d, o = 1, g = 1", cur.L)) +
         scale_colour_manual(labels = c("Binary Broadcast",
@@ -42,7 +44,41 @@ for (cur.L in unique(log.df$L)) {
                                        "Fixed Corrected Tree Broadcast",
                                        "Phased Checked Corrected Tree Broadcast"),
                             values = c(1:4)) +
-        theme(legend.position = c(.85, .35))
+        theme(legend.position = c(.8, .3))
+    print(p)
+}
+
+for (cur.L in unique(log.df$L)) {
+    ymax = max(log.df$MsgTask)
+    p <- ggplot(log.df[L == cur.L], aes(x = P, y = MsgTask, color = Collective)) +
+        geom_point() + geom_line() +
+        scale_x_log10(breaks = node.list - 1, labels = node.list - 1) +
+        ylim(0, ymax) + ylab("Message count") +
+        ggtitle("Scalability in failure free case (log)",
+                subtitle=sprintf("L = %d, o = 1, g = 1", cur.L)) +
+        scale_colour_manual(labels = c("Binary Broadcast",
+                                       "Eager Checked Corrected Tree Broadcast",
+                                       "Fixed Corrected Tree Broadcast",
+                                       "Phased Checked Corrected Tree Broadcast"),
+                            values = c(1:4)) +
+        theme(legend.position = c(.25, .35))
+    print(p)
+}
+
+for (cur.L in unique(log.df$L)) {
+    ymax = max(log.df$MsgTask)
+    p <- ggplot(log.df[L == cur.L], aes(x = P, y = MsgTask, color = Collective)) +
+        geom_point() + geom_line() +
+        scale_x_log10(breaks = node.list - 1, labels = node.list - 1) +
+        scale_y_log10() + ylab("Total number of messages") +
+        ggtitle("Scalability in failure free case (log-log)",
+                subtitle=sprintf("L = %d, o = 1, g = 1", cur.L)) +
+        scale_colour_manual(labels = c("Binary Broadcast",
+                                       "Eager Checked Corrected Tree Broadcast",
+                                       "Fixed Corrected Tree Broadcast",
+                                       "Phased Checked Corrected Tree Broadcast"),
+                            values = c(1:4)) +
+        theme(legend.position = c(.75, .25))
     print(p)
 }
 
