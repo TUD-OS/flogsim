e525be26ba9fdbdcc42ee475210fc82c02fbaaf9
Maksym Planeta <mcsim.planeta@gmail.com>
2017-11-29 15:17:24 +0100

    Catch exceptions in the main function

--- src/flogsim.cpp
+++ src/flogsim.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <cassert>
+#include <exception>
 
 #include <memory>
 #include <iostream>
@@ -19,42 +20,46 @@ class Collective;
 
 int main(int argc, char *argv[])
 {
-  ConfigurationArgs conf(argc, argv);
+  try {
+    ConfigurationArgs conf(argc, argv);
 
-  Model model(conf);
-  Globals::set({&conf, &model});
+    Model model(conf);
+    Globals::set({&conf, &model});
 
-  auto coll = CollectiveRegistry::create();
-  auto faults = FaultInjector::create();
-  TaskQueue tq{faults.get()};
-  Timeline timeline;
+    auto coll = CollectiveRegistry::create();
+    auto faults = FaultInjector::create();
+    TaskQueue tq{faults.get()};
+    Timeline timeline;
 
-  tq.run(*coll, timeline);
+    tq.run(*coll, timeline);
 
-  std::cout << "TotalRuntime," << timeline.get_total_time() << std::endl;
+    std::cout << "TotalRuntime," << timeline.get_total_time() << std::endl;
 
-  auto [failed, finished, unreached] = timeline.node_stat();
-  std::cout << "FailedNodes," << failed << std::endl
-            << "FinishedNodes," << finished << std::endl
-            << "UnreachedNodes," << unreached << std::endl
-            << "MsgTask," << MsgTask::issued() << std::endl
-            << "FailedNodeList," << tq.faults() << std::endl;
+    auto [failed, finished, unreached] = timeline.node_stat();
+    std::cout << "FailedNodes," << failed << std::endl
+              << "FinishedNodes," << finished << std::endl
+              << "UnreachedNodes," << unreached << std::endl
+              << "MsgTask," << MsgTask::issued() << std::endl
+              << "FailedNodeList," << tq.faults() << std::endl;
 
-  if (conf.verbose) {
-    std::cout << "ReschRecvStartTask," << RecvStartTask::reschedules() << std::endl
-              << "ReschRecvEndTask," << RecvEndTask::reschedules() << std::endl
-              << "ReschSendStartTask," << SendStartTask::reschedules() << std::endl
-              << "ReschSendEndTask," << SendEndTask::reschedules() << std::endl
-              << "ReschIdleTask," << IdleTask::reschedules() << std::endl;
+    if (conf.verbose) {
+      std::cout << "ReschRecvStartTask," << RecvStartTask::reschedules() << std::endl
+                << "ReschRecvEndTask," << RecvEndTask::reschedules() << std::endl
+                << "ReschSendStartTask," << SendStartTask::reschedules() << std::endl
+                << "ReschSendEndTask," << SendEndTask::reschedules() << std::endl
+                << "ReschIdleTask," << IdleTask::reschedules() << std::endl;
+    }
+
+    auto trace_filename = conf.log_prefix + ".trace.csv";
+    std::ofstream trace_log(trace_filename);
+    trace_log << timeline;
+
+    auto model_filename = conf.log_prefix + ".model.csv";
+    std::ofstream model_log(model_filename);
+    model_log << model;
+  } catch (const std::exception &e) {
+    std::cerr << "Caught an exception: " << e.what() << std::endl;
   }
-
-  auto trace_filename = conf.log_prefix + ".trace.csv";
-  std::ofstream trace_log(trace_filename);
-  trace_log << timeline;
-
-  auto model_filename = conf.log_prefix + ".model.csv";
-  std::ofstream model_log(model_filename);
-  model_log << model;
 
   return 0;
 }
