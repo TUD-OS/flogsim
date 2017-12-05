c746e783776f546b8ef2bd59d0389f5695564a80
Maksym Planeta <mcsim.planeta@gmail.com>
2017-12-05 17:43:42 +0100

    Change phase end calculation to recursive algorithm
    This algorithm is supposed to give a correct answer even for non-full
    trees.
    
    Nevertheless I found a problem. What if g > o, what should determine
    the end of the phase o or g? If o, then the last node, can't send the
    correction message timely. If g, then we the rest just wastes time not
    sending the message.

--- collectives/phased_checked_correctedtree_bcast.hpp
+++ collectives/phased_checked_correctedtree_bcast.hpp
@@ -14,17 +14,36 @@ class PhasedCheckedCorrectedTreeBroadcast
   // This method ensures that the correction phase starts when tree
   // phase ends. It need to compute right moment of time, when
   // correction may start.
+  static Time __correction_phase_start(auto L, auto o, auto g, int P, int k)
+  {
+    if (P <= 1) {
+      return Time(0);
+    }
+
+    if (P <= k + 1) {
+      return std::max(o, g) * P + L;
+    }
+
+    Time subtree_max(0);
+
+    for (int i = 1; i <= k; i++) {
+      int sub_P = (P - 1) / k + (i <= (P - 1) % k);
+      Time sub_time = __correction_phase_start(L, o, g, sub_P, k);
+      subtree_max = std::max(subtree_max, std::max(o, g) * (i + 1) + L + sub_time);
+    }
+
+    return subtree_max;
+  }
+
   virtual Time correction_phase_start(int k) override
   {
     auto &model = Globals::get().model();
+    auto L = model.L;
     auto o = model.o;
     auto g = model.g;
     auto P = model.P;
 
-    int levels = int(std::floor(std::log(P) / std::log(k)));
-    auto send_time = Time(std::max(o, g) * k);
-    auto recv_time = o;
-    return (send_time + Time(model.L) + recv_time) * levels;
+    return __correction_phase_start(L, o, g, P, k);
   }
 public:
   static constexpr char name[] = "phased_checked_correctedtree_bcast";
