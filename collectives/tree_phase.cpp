4e09a1266958a40533a2461970535936a9b0ca4f
Maksym Planeta <mcsim.planeta@gmail.com>
2017-12-11 14:21:49 +0100

    [Style] Join anonymous namespaces

--- collectives/tree_phase.cpp
+++ collectives/tree_phase.cpp
@@ -7,33 +7,30 @@
 
 using Result = Phase::Result;
 
-
-namespace {
-  // Helper function to calculate runtime of whole tree
-  Time calc_runtime(auto L, auto o, auto g, int P, int k)
-  {
-    if (P <= 1) {
-      return Time(0);
-    }
-
-    if (P <= k + 1) {
-      return std::max(o, g) * P + L;
-    }
-
-    Time subtree_max(0);
-
-    for (int i = 1; i <= k; i++) {
-      int sub_P = (P - 1) / k + (i <= (P - 1) % k);
-      Time sub_time = calc_runtime(L, o, g, sub_P, k);
-      subtree_max = std::max(subtree_max, std::max(o, g) * (i + 1) + L + sub_time);
-    }
-
-    return subtree_max;
-  }
-} // end anon namespace
-
 namespace
 {
+// Helper function to calculate runtime of whole tree
+Time calc_runtime(auto L, auto o, auto g, int P, int k)
+{
+  if (P <= 1) {
+    return Time(0);
+  }
+
+  if (P <= k + 1) {
+    return std::max(o, g) * P + L;
+  }
+
+  Time subtree_max(0);
+
+  for (int i = 1; i <= k; i++) {
+    int sub_P = (P - 1) / k + (i <= (P - 1) % k);
+    Time sub_time = calc_runtime(L, o, g, sub_P, k);
+    subtree_max = std::max(subtree_max, std::max(o, g) * (i + 1) + L + sub_time);
+  }
+
+  return subtree_max;
+}
+
 int get_lvl(int sender, int arity)
 {
   int n_at_lvl = 1;
@@ -46,7 +43,7 @@ int get_lvl(int sender, int arity)
   }
   return lvl;
 }
-}
+} // end anon namespace
 
 template <bool interleave>
 void
