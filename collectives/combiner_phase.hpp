872e06b35c5fa8f06fe34603f3240dea032c9d56
Maksym Planeta <mcsim.planeta@gmail.com>
2017-12-11 15:44:13 +0100

    Add StartPhase

--- collectives/combiner_phase.hpp
+++ collectives/combiner_phase.hpp
@@ -23,3 +23,39 @@ public:
   virtual Result dispatch(const TimerTask &t, TaskQueue &tq, int node_id) override;
   virtual Result dispatch(const RecvEndTask &t, TaskQueue &tq, int node_id) override;
 };
+
+// This class to be used by CombinerPhase as a kick-off to set up
+// reach_nodes. It either sets up the root or enables
+// everybody. Alternatively it accepts explicit list of reached nodes
+// as an initializer list to be used in testing.
+class StartPhase : public Phase
+{
+public:
+  // Enable root
+  StartPhase(Phase::ReachedPtr reached_nodes, int root)
+    : Phase(reached_nodes)
+  {
+    (*reached_nodes)[root] = true;
+  }
+
+  // Enable everybody
+  StartPhase(Phase::ReachedPtr reached_nodes)
+    : Phase(reached_nodes)
+  {
+    std::fill(reached_nodes->begin(), reached_nodes->end(), true);
+  }
+
+  // Enable selected
+  StartPhase(Phase::ReachedPtr reached_nodes, std::initializer_list<int> selected)
+    : Phase(reached_nodes)
+  {
+    for (auto i : selected) {
+      (*reached_nodes)[i] = true;
+    }
+  }
+
+  virtual Result dispatch(const InitTask &, TaskQueue &tq, int node_id) override
+  {
+    return Result::DONE_PHASE;
+  }
+};
