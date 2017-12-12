0a926ba3e095f2087115d5e57067c4258eec77ba
Maksym Planeta <mcsim.planeta@gmail.com>
2017-12-13 00:32:10 +0100

    Remove deadline from exclusive phase
    This effectively prohibits nesting of exclusive phases

--- collectives/exclusive_phase.hpp
+++ collectives/exclusive_phase.hpp
@@ -22,6 +22,4 @@ public:
   virtual Result dispatch(const IdleTask &t, TaskQueue &tq, int node_id) override;
   virtual Result dispatch(const TimerTask &t, TaskQueue &tq, int node_id) override;
   virtual Result dispatch(const RecvEndTask &t, TaskQueue &tq, int node_id) override;
-
-  virtual Time deadline() const;
 };
