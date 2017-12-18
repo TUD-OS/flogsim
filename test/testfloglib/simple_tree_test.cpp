852d6a716a950dbe6e123415e8de3078c70a15b2
Maksym Planeta <mcsim.planeta@gmail.com>
2017-12-18 20:11:42 +0100

    Rework tests even more

--- test/testfloglib/simple_tree_test.cpp
+++ test/testfloglib/simple_tree_test.cpp
@@ -1,23 +1,30 @@
 #include "globals.hpp"
 
 #include "test_wrappers.hpp"
+#include "kary_tree.hpp"
 
-#define ALGORITHM SimpleTreeBroadcast
 namespace
 {
 
-TEST(ALGORITHM, Functional)
+TEST_P(PhaseTest, Runtime)
 {
-
-  // CALL(NoFaultTest<ALGORITHM>().LogP(1, 1, 1, 4 ).k(3).runtime(5));
-  // CALL(NoFaultTest<ALGORITHM>().LogP(1, 1, 1, 13).k(3).runtime(10));
-  // CALL(NoFaultTest<ALGORITHM>().LogP(1, 1, 1, 5 ).k(4).runtime(6));
-
-  // CALL(FaultTest<ALGORITHM>().LogP(1, 1, 1, 4).k(3).
-  //      failed({0}).runtime(0).unreach(3));
-  // CALL(FaultTest<ALGORITHM>().LogP(1, 1, 1, 4).k(3).
-  //      failed({1}).runtime(5));
-
+  do_test(
+    [](ReachedNodes& rn)
+    {
+      return std::make_unique<KAryTreePhase<true>>(rn);
+    });
 }
 
+RunParams KaryTreeTests[] = {
+  RunParams().LogP(1, 1, 1, 4 ).k(3).runtime(5),
+  RunParams().LogP(1, 1, 1, 13).k(3).runtime(10),
+  RunParams().LogP(1, 1, 1, 5 ).k(4).runtime(6),
+  RunParams().LogP(1, 1, 1, 4).k(3).failed({0}).runtime(0).unreach(3),
+  RunParams().LogP(1, 1, 1, 4).k(3).failed({1}).runtime(5),
+};
+
+
+INSTANTIATE_TEST_CASE_P(KaryTreeTests,
+                        PhaseTest,
+                        ::testing::ValuesIn(KaryTreeTests));
 }
--- test/testfloglib/test_wrappers.hpp
+++ test/testfloglib/test_wrappers.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <functional>
+#include <fstream>
+
+#include <experimental/filesystem>
 
 #include "gtest/gtest.h"
 #include "fault_injector.hpp"
@@ -8,9 +11,10 @@
 #include "collective.hpp"
 #include "phase.hpp"
 
-class PhaseTest
+using create_coll_t = std::function<std::unique_ptr<Phase>(ReachedNodes&)>;
+
+struct RunParams
 {
-protected:
   int param_L, param_o, param_g;
   int param_P;
   // Arity
@@ -22,35 +26,17 @@ protected:
   // List of failed nodes
   std::vector<int> param_failed;
   // Factory method for creating collectives
-  std::function<std::unique_ptr<Phase>(ReachedNodes&)> create_coll;
+  create_coll_t create_coll;
 
-  std::string make_command(const Configuration &conf, const FaultInjector &fi)
+  friend std::ostream &operator<<(std::ostream &os, const RunParams &rp)
   {
-    std::stringstream ss;
-
-    ss << "./flogsim --k " << conf.k
-       << " --L " << conf.L
-       << " --o " << conf.o
-       << " --g " << conf.g
-       << " --P " << conf.P
-       << " --faults " << fi
-       << " --coll " << "whatever"
-       << " --prio " << conf.priority;
-    return ss.str();
-  }
-
-public:
-  auto &init()
-  {
-    param_L = 1;
-    param_o = 1;
-    param_g = 1;
-    param_P = 1;
-    param_k = 1;
-    expect_runtime = 0;
-    expect_unreach = 0;
-    param_failed = decltype(param_failed)();
-    return *this;
+    os << "./flogsim --k " << rp.param_k
+       << " --L " << rp.param_L
+       << " --o " << rp.param_o
+       << " --g " << rp.param_g
+       << " --P " << rp.param_P
+       << " --coll " << "whatever";
+    return os;
   }
 
   auto &L(int L)
@@ -106,51 +92,101 @@ public:
     return *this;
   }
 
-  PhaseTest(auto create_coll)
-    : create_coll(create_coll)
-  {
-    init();
-  }
+  RunParams(const RunParams&) = default;
+  RunParams(RunParams* const rp) : RunParams(*rp) {}
 
-  void operator()(int linenum)
+  RunParams()
+    : param_L(1),
+      param_o(1),
+      param_g(1),
+      param_P(1),
+      param_k(1),
+      expect_runtime(0),
+      expect_unreach(0),
+      create_coll(nullptr)
+  {}
+
+};
+
+namespace std
+{
+template<>
+struct hash<RunParams>
+{
+  typedef RunParams argument_type;
+  typedef std::size_t result_type;
+  result_type operator()(argument_type const &rp) const noexcept
   {
-    auto conf = Configuration(this->param_k,
-                              this->expect_runtime + 10).
-      LogP(this->param_L,
-           this->param_o,
-           this->param_g,
-           this->param_P).faults("none");
+    result_type h = ((std::hash<int>{}(rp.param_L)) ^
+                     (std::hash<int>{}(rp.param_o) << 1) ^
+                     (std::hash<int>{}(rp.param_g) << 2) ^
+                     (std::hash<int>{}(rp.param_P) << 3) ^
+                     (std::hash<int>{}(rp.param_k) << 3));
+    std::for_each(rp.param_failed.begin(), rp.param_failed.end(),
+                  [&](const auto &e) {
+                    h = (h << 1) ^ std::hash<int>{}(e);
+                  });
+    typedef void function_t(void) ;
+    h = (h << 1) ^ std::hash<function_t*>{}(rp.create_coll.target<function_t>());
+    return h;
+  }
+};
+}
+
+class PhaseTest : public ::testing::TestWithParam<RunParams>
+{
+public:
+  void do_test(create_coll_t create_coll)
+  {
+    namespace fs = std::experimental::filesystem;
+
+    RunParams param = GetParam();
+
+    param.create_coll = create_coll;
+
+    fs::path log_dir = "./logs/";
+    std::string log_prefix = log_dir.c_str() + std::to_string(std::hash<RunParams>{}(param));
+    fs::path trace_filename(log_prefix + ".trace.csv");
+    fs::path model_filename(log_prefix + ".model.csv");
+
+    std::string info("Check " +
+                     trace_filename.native() + " and " +
+                     model_filename.native());
+
+    auto conf = Configuration(param.param_k, param.expect_runtime + 10).
+      LogP(param.param_L,
+           param.param_o,
+           param.param_g,
+           param.param_P).faults("none");
+
     auto model = Model(conf);
 
     Globals::set({&conf, &model});
 
-    auto coll = Collective({0}, std::make_unique<ListFaults>(this->param_failed));
+    auto coll = Collective({0}, std::make_unique<ListFaults>(param.param_failed));
 
-    auto timeline = coll.run(this->create_coll(coll.reached_nodes));
-
-    auto cmd = this->make_command(conf, *coll.faults.get());
-    std::stringstream info;
-    info << conf << std::endl
-         << "Test case from line: " << linenum << std::endl
-         << "Suggested command:" << std::endl
-         << cmd;
-    auto info_str = info.str();
+    auto timeline = coll.run(create_coll(coll.reached_nodes));
 
     EXPECT_EQ(timeline.get_total_time(),
-              Time(this->expect_runtime)) << info_str;
+              Time(param.expect_runtime)) << info;
 
     auto [failed, finished, unreached] = timeline.node_stat();
     int fault_count = coll.faults->fault_count();
-    auto expect_finished = (this->param_P - fault_count -
-                            this->expect_unreach);
-    EXPECT_EQ(failed, fault_count) << info_str;
-    EXPECT_EQ(finished, expect_finished) << info_str;
-    EXPECT_EQ(unreached, this->expect_unreach) << info_str;
+    auto expect_finished = (param.param_P - fault_count -
+                            param.expect_unreach);
+    EXPECT_EQ(failed, fault_count) << info;
+    EXPECT_EQ(finished, expect_finished) << info;
+    EXPECT_EQ(unreached, param.expect_unreach) << info;
+
+    if (!fs::exists(log_dir)) {
+      fs::create_directory(log_dir);
+    } else if (!fs::is_directory(log_dir)) {
+      FAIL() << "Not a log directory: " << log_dir;
+    }
+    std::ofstream trace_log(trace_filename);
+    trace_log << timeline;
+
+    std::ofstream model_log(model_filename);
+    model_log << model;
   }
-
 };
-
-#define CALL(t, x)                              \
-  ({                                            \
-    t.init().x(__LINE__);                       \
-  })
