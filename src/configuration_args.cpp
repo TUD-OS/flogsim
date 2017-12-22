70dc98a9ec55c3174c94cd5990aeb1728a45b63f
Maksym Planeta <mcsim.planeta@gmail.com>
2017-12-22 10:53:27 +0100

    Small refactoring inside configuration args

--- src/configuration_args.cpp
+++ src/configuration_args.cpp
@@ -25,6 +25,9 @@ void validate(boost::any& v,
   }
 }
 
+namespace
+{
+
 void
 show_help(const po::options_description& desc)
 {
@@ -32,49 +35,49 @@ show_help(const po::options_description& desc)
     exit( EXIT_SUCCESS );
 }
 
-ConfigurationArgs::ConfigurationArgs(int argc, char *argv[])
+po::options_description create_options_description(ConfigurationArgs &conf)
 {
   po::options_description desc("General options");
   desc.add_options()
     ("help", "Show help message")
     ("log",
-     po::value<std::string>(&log_prefix)->value_name("PREFIX"),
+     po::value<std::string>(&conf.log_prefix)->value_name("PREFIX"),
      "Where to store the logs. Adds suffixes '.model.csv' and '.trace.csv' to the output files.")
     ("results-format",
      po::value<std::string>()->value_name("NAME")->default_value("tables"),
      "Format to print out runtime results. Allowed values: tables, csv, csv-id.")
     ("id",
-     po::value<std::string>(&id)->value_name("ID"),
+     po::value<std::string>(&conf.id)->value_name("ID"),
      "Id of the experiment. Used together with 'csv-id' format.")
     ("time_limit",
-     po::value<int64_t>(&limit)->value_name("TIME"),
+     po::value<int64_t>(&conf.limit)->value_name("TIME"),
      "When to stop the simulation.")
     ("verbose,v",
-     po::bool_switch(&verbose)->default_value(false),
+     po::bool_switch(&conf.verbose)->default_value(false),
      "Be verbose")
     ;
 
   po::options_description model("Model options");
   model.add_options()
     ("latency,L",
-     po::value<int>(&L)->default_value(1)->value_name("TIME"),
+     po::value<int>(&conf.L)->default_value(1)->value_name("TIME"),
      "Network latency")
     ("overhead,o",
-     po::value<int>(&o)->default_value(1)->value_name("TIME"),
+     po::value<int>(&conf.o)->default_value(1)->value_name("TIME"),
      "CPU overhead")
     ("gap,g",
-     po::value<int>(&g)->default_value(2)->value_name("TIME"),
+     po::value<int>(&conf.g)->default_value(2)->value_name("TIME"),
      "Send/Recv gap")
     ("processors,P",
-     po::value<int>(&P)->default_value(8)->value_name("NUM"),
+     po::value<int>(&conf.P)->default_value(8)->value_name("NUM"),
      "Number of processors")
     ("prio",
-     po::value<Configuration::Priority>(&priority)->
+     po::value<Configuration::Priority>(&conf.priority)->
      default_value(Configuration::Priority("recv"))->value_name("NAME"),
      "How to schedule tasks on a CPU. Option 'recv' prefers receives "
      "over sends. Option 'tag' looks at the tag first.")
     ("parallelism",
-     po::value<int>(&parallelism)->default_value(1)->value_name("NUM"),
+     po::value<int>(&conf.parallelism)->default_value(1)->value_name("NUM"),
      "Parallelism level per node."
      " Level 2 allows simulteneous sending and receiving."
      " By LogP should be capped by ceil(L/g), but not in our case.")
@@ -85,11 +88,11 @@ ConfigurationArgs::ConfigurationArgs(int argc, char *argv[])
   po::options_description faults("Fault injector options");
   faults.add_options()
     ("faults",
-     po::value<std::string>(&fault_injector)->
+     po::value<std::string>(&conf.fault_injector)->
      default_value("none")->value_name("NAME"),
      "Type of fault injector")
     ("fault-count,F",
-     po::value<int>(&F)->default_value(0)->value_name("NUM"),
+     po::value<int>(&conf.F)->default_value(0)->value_name("NUM"),
      "Number of faults")
     ;
 
@@ -98,50 +101,42 @@ ConfigurationArgs::ConfigurationArgs(int argc, char *argv[])
   po::options_description collectives("Collectives options");
   collectives.add_options()
     ("coll",
-     po::value<std::string>(&collective)->
+     po::value<std::string>(&conf.collective)->
      default_value("binary_bcast")->value_name("NAME"),
      "Type of collective to model")
     ("karity,k",
-     po::value<int>(&k)->default_value(2)->value_name("NUM"),
+     po::value<int>(&conf.k)->default_value(2)->value_name("NUM"),
      "K-arity of the tree")
     ("seed",
-     po::value<unsigned>(&seed)->default_value(0)->value_name("NUM"),
+     po::value<unsigned>(&conf.seed)->default_value(0)->value_name("NUM"),
      "Seed for the entropy source. Value 0 tells the simulator to generate "
      "seed on its own.")
     ;
 
   desc.add(collectives);
+  return desc;
+}
 
-  po::variables_map args;
-
-  try {
-    po::store(
-      po::parse_command_line(argc, argv, desc),
-      args);
-  } catch (po::error const& e) {
-    std::cerr << e.what() << '\n';
-    exit(EXIT_FAILURE);
-  }
-  po::notify(args);
-
+void validate_options(ConfigurationArgs &conf, po::variables_map &args)
+{
   if (!args.count("log")) {
     assert(log_prefix == "");
   }
 
   // Set default value for time limit
   if (!args.count("time_limit")) {
-    limit = INT64_MAX;
+    conf.limit = INT64_MAX;
   }
 
   // Check specification for results format
   bool need_id = false;
   if (args.count("results-format")) {
-    results_format = args["results-format"].as<std::string>();
-    if (results_format == "table") {
+    conf.results_format = args["results-format"].as<std::string>();
+    if (conf.results_format == "table") {
       // OK
-    } else if (results_format == "csv") {
+    } else if (conf.results_format == "csv") {
       // OK
-    } else if (results_format == "csv-id") {
+    } else if (conf.results_format == "csv-id") {
       if (args.count("id")) {
         // OK
         need_id = true;
@@ -151,16 +146,37 @@ ConfigurationArgs::ConfigurationArgs(int argc, char *argv[])
         throw po::validation_error(po::validation_error::invalid_option_value);
       }
     } else {
-      std::cerr << "Unknown output format: " << results_format << std::endl;
+      std::cerr << "Unknown output format: "
+                << conf.results_format << std::endl;
       throw po::validation_error(po::validation_error::invalid_option_value);
     }
   } else {
-    results_format = "table";
+    conf.results_format = "table";
   }
 
   if (args.count("id") && !need_id) {
     std::cerr << "Unexpected parameter --id." << std::endl;
     throw po::validation_error(po::validation_error::invalid_option_value);
+  }
+}
+
+}
+
+ConfigurationArgs::ConfigurationArgs(int argc, char *argv[])
+{
+  auto desc = create_options_description(*this);
+
+  po::variables_map args;
+
+  try {
+    po::store(po::parse_command_line(argc, argv, desc), args);
+
+    po::notify(args);
+
+    validate_options(*this, args);
+  } catch (po::error const& e) {
+    std::cerr << "Exit reason: " << e.what() << '\n';
+    show_help(desc);
   }
 
   if (args.count("help")) {
