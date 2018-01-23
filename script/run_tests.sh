ea24f3a8f8f23fcfd0144156027b9c4a76280d20
Maksym Planeta <mcsim.planeta@gmail.com>
2018-01-23 23:46:50 +0100

    Update run_tests.sh

--- script/run_tests.sh
+++ script/run_tests.sh
@@ -10,15 +10,25 @@ ALL_COLL="{checked_corrected_binomial_bcast,phased_checked_corrected_binomial_bc
 ALL_L="{1,2}"
 ALL_PAR="{1,2}"
 ALL_K="2"
+ALL_D="2"
 
-COMBINATIONS=$(eval echo "$ALL_COLL+$ALL_L+$ALL_P+$ALL_PAR+$ALL_K")
+COMBINATIONS=$(eval echo "$ALL_COLL+$ALL_L+$ALL_P+$ALL_PAR+$ALL_K+$ALL_D")
 
 ALL_COLL="{phased_checked_corrected_kary_bcast,checked_corrected_kary_bcast,checked_always_corrected_kary_bcast}"
 ALL_K="4"
-COMBINATIONS="$COMBINATIONS "$(eval echo "$ALL_COLL+$ALL_L+$ALL_P+$ALL_PAR+$ALL_K")
+COMBINATIONS="$COMBINATIONS "$(eval echo "$ALL_COLL+$ALL_L+$ALL_P+$ALL_PAR+$ALL_K+$ALL_D")
+
+ALL_COLL="{optimized_opportunistic_corrected_kary_bcast,optimized_opportunistic_corrected_binomial_bcast,optimized_opportunistic_corrected_optimal_bcast}"
+ALL_K="4"
+ALL_D="{2,4}"
+COMBINATIONS="$COMBINATIONS "$(eval echo "$ALL_COLL+$ALL_L+$ALL_P+$ALL_PAR+$ALL_K+$ALL_D")
+
+ALL_COLL="optimized_opportunistic_corrected_lame_bcast"
+ALL_K="2"
+COMBINATIONS="$COMBINATIONS "$(eval echo "$ALL_COLL+$ALL_L+$ALL_P+$ALL_PAR+$ALL_K+$ALL_D")
 
 for EXPERIMENT in $COMBINATIONS
 do
-    read COLL L P PAR k <<<$(IFS="+"; echo $EXPERIMENT)
-    ./flogsim --parallelism $PAR -k $k -P $P -o 1 -g 1 -L $L --faults none --coll $COLL --results-format csv -r 1 | tail -n 1
+    read COLL L P PAR k d <<<$(IFS="+"; echo $EXPERIMENT)
+    ./flogsim --parallelism $PAR -d $d -k $k -P $P -o 1 -g 1 -L $L --faults none --coll $COLL --results-format csv -r 1 | tail -n 1
 done
